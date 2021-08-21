1dc76a437cb589eb50ea02558fa9f7b970ff631e
Joakim Wennergren <joakim@bitsanddivs.com>
2021-08-21 14:33:23 +0200

    Initial Commit

---
+++ Chord Ruler/Chord_RulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chord_RulerApp.swift
+//  Chord Ruler
+//
+//  Created by Joakim Wennergren on 2021-08-21.
+//
+
+import SwiftUI
+
+@main
+struct Chord_RulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
